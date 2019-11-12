e7a0433de555d966a29db5f70f04058324d01f4a
shibatamasayuki <masayuki.shibata.0831@gmail.com>
2019-11-12 22:42:51 +0900

    削除､編集､検索､重複を許可しない機能を追加

---
+++ crm_app.py
@@ -0,0 +1,116 @@
+import sqlite3
+
+
+def main():
+    print('===== Welcome to CRM Application ====='
+          '\n[S]how: Show all users info'
+          '\n[A]dd: Add new user'
+          '\n======================================')
+    while True:
+        select_menu = input()
+
+        if select_menu == 'A' or select_menu == 'a':
+            print(f'Your command > {select_menu}')
+            name = input('New user name > ')
+            age = input('New user age > ')
+
+            try:
+                register_user(name, age)
+                print(f'Add new user: {name}')
+
+            except sqlite3.IntegrityError:
+                print(f'Duplicated user name {name}')
+
+
+        elif select_menu == 'S' or select_menu == 's':
+            print(f'Your command > {select_menu}')
+            users = show_users()
+
+            for user in users:
+                print(f'Name: {user[0]}, Age: {user[1]}')
+
+        elif select_menu == 'F' or select_menu == 'f':
+            print(f'Your command > {select_menu}')
+            find_name = input('User name > ')
+
+            try:
+                print(f'Name: {find_user(find_name)[0]} Age: {find_user(find_name)[1]}')
+            except TypeError:
+                print(f'Sorry, {find_name} is not found')
+
+        elif select_menu == 'D' or select_menu == 'd':
+            print(f'Your command > {select_menu}')
+            delete_name = input('User name > ')
+
+            delete_user(delete_name)
+            print(f'{delete_name} is deleted')
+
+        elif select_menu == 'E' or select_menu == 'e':
+
+            pre_edit_name = input('User name > ')
+
+            try:
+                edit_name = input(f'New user name ({find_user(pre_edit_name)[0]})  > ')
+                edit_age = input(f'New user age ({find_user(pre_edit_name)[1]})  > ')
+                edit_user_name(pre_edit_name, edit_name, edit_age)
+            except TypeError:
+                print(f'Sorry, {pre_edit_name} is not found')
+
+        elif select_menu == 'Q' or select_menu == 'q':
+            print(f'Your command > {select_menu}')
+            print(f'Bye!')
+            break
+
+
+        else:
+            print(f'Your command > {select_menu}')
+            print(f'{select_menu}: command not found')
+
+
+def register_user(name, age):
+    connection = sqlite3.connect('crm.db')
+    cursor = connection.cursor()
+    sql = f"INSERT INTO users (name,age) VALUES(?,?)"
+    cursor.execute(sql, (name, age))
+    connection.commit()
+    connection.close()
+
+
+def show_users():
+    connection = sqlite3.connect('crm.db')
+    cursor = connection.cursor()
+    sql = f"SELECT * FROM users"
+    users = cursor.execute(sql).fetchall()
+    connection.close()
+    return users
+
+
+def delete_user(delete_name):
+    connection = sqlite3.connect('crm.db')
+    cursor = connection.cursor()
+    sql = f"DELETE FROM users  WHERE name = ?"
+    cursor.execute(sql, (delete_name,))
+    connection.commit()
+    connection.close()
+
+
+def find_user(find_name):
+    connection = sqlite3.connect('crm.db')
+    cursor = connection.cursor()
+    sql = f"SELECT * FROM users  WHERE name == '{find_name}'"
+    find_user = cursor.execute(sql).fetchone()
+    connection.close()
+    return find_user
+
+
+def edit_user_name(pre_edit_name, edit_name, edit_age):
+    connection = sqlite3.connect('crm.db')
+    cursor = connection.cursor()
+    sql = f"UPDATE users SET name = '{edit_name}',age = '{edit_age}'  WHERE name = '{pre_edit_name}'"
+    cursor.execute(sql)
+    connection.commit()
+    connection.close()
+
+
+if __name__ == '__main__':
+    main()
